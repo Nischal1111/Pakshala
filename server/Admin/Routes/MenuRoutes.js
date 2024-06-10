bfecd8ad69a9fa7e408fac2070ebe295c31a83da
itsmesauravk <sauravkarki10.12@gmail.com>
2024-06-10 22:29:44 +0545

    started backend server

---
+++ server/Admin/Routes/MenuRoutes.js
@@ -0,0 +1,22 @@
+
+const express = require('express');
+const adminRoutes = express.Router();
+
+const multer = require('multer');
+
+const uploader= multer({
+    storage: multer.diskStorage({})
+});
+
+
+const {
+  addMenuItem  
+} = require('../Controllers/MenuControl');
+
+
+// adding menu item
+adminRoutes.post('/add-menu-item', uploader.single('menu') ,addMenuItem);
+
+
+
+module.exports = adminRoutes;
---
+++ server/Middlewares/UploadFile.js
@@ -0,0 +1,32 @@
+// import {v2 as cloudinary} from 'cloudinary';
+const cloudinary = require('cloudinary').v2;
+
+// Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+if(!process.env.CLOUDINARY_CLOUD_NAME || !process.env.CLOUDINARY_API_KEY || !process.env.CLOUDINARY_API_SECRET) {
+    console.error('Cloudinary config not found');
+    process.exit(1);
+}
+
+const uploadFile = async (file) => {
+    try {
+        const uploadResult = await cloudinary.uploader.upload(file, {
+            folder:"menus"
+        });
+        if(!uploadResult) {
+            throw new Error('Error uploading file');
+        }
+        console.log(uploadResult);
+        return uploadResult;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = uploadFile;
+
